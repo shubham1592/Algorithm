d8995c8c5e1eadccd5f9aec94e8d0363e9051b8f
Shubham Kumar <pi.shubham1592@gmail.com>
2020-10-25 02:11:28 +0530

    added stack reversal

---
+++ stack/stack.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void transfer(stack<int> &s1, stack<int> &s2, int n){
+    for(int i=0; i<n; i++){
+        s2.push(s1.top());
+        s1.pop();
+    }
+}
+
+void reverses(stack<int> &s1){
+    stack<int> s2;
+    int n = s1.size();
+    for(int i=0; i<n; i++){
+        int x = s1.top();
+        s1.pop();
+        transfer(s1,s2,n-i-1);
+        s1.push(x);
+        transfer(s2,s1,n-i-1);
+    }
+}
+
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    reverses(s);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+
+}
