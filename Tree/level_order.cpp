0c873b255e401b4dd1b186e88a5ff16447857431
Shubham Kumar <pi.shubham1592@gmail.com>
2020-10-30 22:51:08 +0530

    level order traversal in a binary tree

---
+++ Tree/level_order.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* build(){
+    int d;
+    cin >> d;
+    if(d==-1){
+        return NULL;
+    }
+    node *root = new node(d);
+    root->left = build();
+    root->right = build();
+    return root;
+}
+
+void preorder_print(node*root){
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preorder_print(root->left);
+    preorder_print(root->right);
+}
+
+void inorder_print(node*root){
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder_print(root->left);
+    cout << root->data << " ";
+    inorder_print(root->right);
+}
+
+void postorder_print(node*root){
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder_print(root->left);
+    postorder_print(root->right);
+    cout << root->data << " ";
+}
+
+int height(node*root){
+    if(root==NULL){
+        return 0;
+    }
+    int ls = height(root->left);
+    int rs = height(root->right);
+    return max(ls, rs) + 1;
+}
+
+void print_kth_level(node*root, int k){
+    if(root==NULL){
+        return;
+    }
+    if(k==1){
+        cout << root->data << " ";
+        return;
+    }
+    print_kth_level(root->left, k-1);
+    print_kth_level(root->right, k-1);
+    return;
+}
+
+void print_level_order(node*root){
+    int h = height(root);
+    for (int i = 1; i <= h; i++){
+        print_kth_level(root, i);
+        cout << endl;
+    }
+    return;
+}
+
+int main(){
+    node*root=build();
+    cout << endl
+         << "Height of the tree: " << endl;
+    cout << height(root);
+    cout << endl;
+    print_kth_level(root, 6);
+    cout << endl;
+    cout <<"Level order print: "<<endl;
+    print_level_order(root);
+    return 0;
+}
