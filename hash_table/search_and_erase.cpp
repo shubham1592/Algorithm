9f12dd422bcf0b99f6ee64d20998a1abc8642734
Shubham Kumar <pi.shubham1592@gmail.com>
2020-12-06 20:45:45 +0530

    added a search functionality to the hash table

---
+++ hash_table/search_and_erase.cpp
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<cstring>
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class node{
+    public:
+        string key;
+        T value;
+        node<T> *next;
+
+        node(string key, T val){
+            this->key = key;
+            value = val;
+            next = NULL;
+        }
+
+        ~node(){
+            if(next!=NULL){
+                delete next;
+            }
+        }
+};
+
+template<typename T>
+class hashtable{
+    node<T> **table;
+    int current_size;
+    int table_size;
+
+    int hashfn(string key){
+        int idx = 0;
+        int p = 1;
+        for (int j = 0; j < key.length(); j++){
+            idx = idx + (key[j] * p) % table_size;
+            idx = idx % table_size;
+            p = (p * 27) % table_size;
+        }
+        return idx;
+    }
+
+    public:
+    hashtable(int ts=7){
+        table_size = ts;
+        table = new node<T> *[table_size];
+        current_size = 0;
+        for(int i = 0; i < table_size; i++){
+            table[i] = NULL;
+        }
+    }
+
+    void insert(string key, T value){
+        int idx = hashfn(key);
+        node<T> *n = new node<T>(key, value);
+        n->next = table[idx];
+        table[idx] = n;
+        current_size++;
+    }
+
+    void print(){
+        for (int i = 0; i < table_size; i++){
+            cout << "Bucket " << i << " -> ";
+            node<T> *temp = table[i];
+            while(temp!=NULL){
+                cout << temp->key << " -> ";
+                temp = temp->next; 
+            }
+            cout << endl;
+        }
+    }
+
+    T *search(string key){
+        int idx = hashfn(key);
+        node<T> *temp = table[idx];
+        while(temp!=NULL){
+            if(temp->key==key){
+                return &temp->value;
+            }
+            temp = temp->next;
+        }
+        return NULL;
+    }
+
+    // void erase(string key){
+        
+    // }
+};
+
+int main(){
+    hashtable<int> price_menu;
+    price_menu.insert("Burger", 60);
+    price_menu.insert("Noodles", 80);
+    price_menu.insert("Sandwich", 45);
+    price_menu.insert("Momos", 50);
+    price_menu.insert("Coke", 30);
+    price_menu.insert("Coffee", 100);
+    price_menu.insert("Pizza", 150);
+
+    price_menu.print();
+    cout << endl;
+
+    int exit = 1;
+    while(exit>0){
+        string input;
+        cout << "Enter the item you want to search for: ";
+        cin >> input;
+        int *price = price_menu.search(input);
+        if(price==NULL){
+            cout << endl << "Item not found";
+        }
+        else{
+            cout << endl << "Price of " << input << " is -> " << *price << endl;
+        }
+        cout << endl;
+        cout << "Enter a positive number to continue testing: ";
+        cin >> exit;
+    }
+    
+    return 0;
+}
