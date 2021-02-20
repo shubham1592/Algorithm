3d9733955ecd5a004a920441482d83c26c476c49
Shubham Kumar <pi.shubham1592@gmail.com>
2021-02-20 22:42:20 +0530

    longest common subsequence #DP

--- DP/longest_common_subsequence.cpp
+++ DP/longest_common_subsequence.cpp
@@ -2,24 +2,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int lcs(string s1, string s2, int i, int j){
+int lcs(string s1, string s2, int i, int j, vector<vector<int>> &dp){
     
     //base case
     if(i == s1.length() or j == s2.length()){
         return 0;
     }
 
+    //DP look-Up
+     if(dp[i][j] != -1){
+         return dp[i][j];
+     }
+
     //rec case
     if(s1[i] == s2[j]){
-        return 1 + lcs(s1, s2, i + 1, j + 1);
+        return dp[i][j] = 1 + lcs(s1, s2, i + 1, j + 1, dp);
     }
-    int op1 = lcs(s1, s2, i + 1, j);
-    int op2 = lcs(s1, s2, i, j + 1);
-    return max(op1, op2);
+    int op1 = lcs(s1, s2, i + 1, j, dp);
+    int op2 = lcs(s1, s2, i, j + 1, dp);
+    return dp[i][j] = max(op1, op2);
 }
 
 int main(){
     string s1 = "ABCDE";
-    string s2 = "ABCjkEllDC";
-    cout << lcs(s1, s2, 0, 0) << endl;
+    string s2 = "ABCDjkEllDC";
+
+    int n1 = s1.length();
+    int n2 = s2.length();
+    vector<vector<int>> dp(n1, vector<int>(n2, -1));
+
+    cout << lcs(s1, s2, 0, 0, dp) << endl;
+
 }
